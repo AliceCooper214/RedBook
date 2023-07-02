d8706e611873ef1bb63444c30b357df3300f218c
cooper <openbilibili@126.com>
2023-07-03 00:03:52 +0800

    chore: init

---
+++ App.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import {StatusBar} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {NavigationContainer} from '@react-navigation/native';
+import {TransitionPresets, createStackNavigator} from '@react-navigation/stack';
+
+import Welcome from './src/modules/welcome/Welcome';
+import Login from './src/modules/login/Login';
+import MainTab from './src/modules/mainTab/MainTab';
+import ArticleDetail from './src/modules/articleDetail/ArticleDetail';
+import SearchGoods from './src/modules/searchGoods/SearchGoods';
+
+const Stack = createStackNavigator();
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaProvider>
+      <StatusBar barStyle={'dark-content'} backgroundColor={'white'} />
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Welcome"
+          screenOptions={{
+            cardStyle: {
+              elevation: 1,
+            },
+          }}>
+          <Stack.Screen
+            name="Welcome"
+            component={Welcome}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{
+              headerShown: false,
+              ...TransitionPresets.SlideFromRightIOS,
+            }}
+          />
+          <Stack.Screen
+            name="MainTab"
+            component={MainTab}
+            options={{
+              headerShown: false,
+              ...TransitionPresets.SlideFromRightIOS,
+            }}
+          />
+          <Stack.Screen
+            name="ArticleDetail"
+            component={ArticleDetail}
+            options={{
+              headerShown: false,
+              ...TransitionPresets.SlideFromRightIOS,
+            }}
+          />
+          <Stack.Screen
+            name="SearchGoods"
+            component={SearchGoods}
+            options={{
+              headerShown: false,
+              presentation: 'transparentModal',
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+}
+
+export default App;
